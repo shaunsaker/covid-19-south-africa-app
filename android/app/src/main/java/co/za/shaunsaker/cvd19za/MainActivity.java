e6bafd2ac45a32cc2a08a845d663d738de6d2cbf
Shaun Saker <shaun@MACs-MacBook-Pro.local>
2020-03-28 21:59:37 +0200

    Rename project

--- android/app/src/main/java/co/za/shaunsaker/cvd19za/MainActivity.java
+++ android/app/src/main/java/co/za/shaunsaker/cvd19za/MainActivity.java
@@ -1,4 +1,4 @@
-package com.temp;
+package co.za.shaunsaker.cvd19za;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "temp";
+    return "CVD19ZA";
   }
 }
--- android/app/src/main/java/co/za/shaunsaker/cvd19za/MainApplication.java
+++ android/app/src/main/java/co/za/shaunsaker/cvd19za/MainApplication.java
@@ -1,4 +1,4 @@
-package com.temp;
+package co.za.shaunsaker.cvd19za;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.temp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("co.za.shaunsaker.cvd19za.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
