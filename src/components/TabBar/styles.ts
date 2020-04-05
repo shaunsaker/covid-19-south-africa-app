77f748a14c9a47db473fb1ea3ea893d2bec35c0e
shaunsaker <shaun@MACs-MacBook-Pro.local>
2020-04-05 17:58:02 +0200

    Increase height of TabBar on tablets

--- src/components/TabBar/styles.ts
+++ src/components/TabBar/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
 
 import {rhythm, colors, getTextShadow} from '../../styleConstants';
+
+const isTablet = Dimensions.get('window').width >= 768;
 
 export const Container = styled.View`
   flex-direction: row;
@@ -15,7 +18,7 @@ export const TabContainer = styled.TouchableOpacity<TabContainerProps>`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: ${rhythm.vt}px ${rhythm.hz / 2}px;
+  padding: ${isTablet ? rhythm.vt * 2 : rhythm.vt}px ${rhythm.hz / 2}px;
   border-top-width: 4px;
   border-top-color: ${({isActive}) =>
     isActive ? colors.primaryText : colors.primaryLight};
