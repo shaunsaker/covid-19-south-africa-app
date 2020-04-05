b860a6a1e361d670c6eced5d86f9382a947417af
shaunsaker <shaun@MACs-MacBook-Pro.local>
2020-04-05 08:31:42 +0200

    Add container to CounterView component

---
+++ src/components/CounterView/CounterView.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Counter from 'react-native-counter';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {View} from 'react-native';
+import Animator from 'react-native-simple-animators';
+
+import styles, {
+  Container,
+  TitleText,
+  FooterText,
+  SourceText,
+  SourceButton,
+  LoadingContainer,
+} from './styles';
+
+export interface Props {
+  title: string;
+  start: Number;
+  end: Number;
+  lastUpdated: string;
+  loading?: boolean;
+  handleCountComplete: () => void;
+  handleSourcePress: () => void;
+}
+
+const CounterView = ({
+  title,
+  start,
+  end,
+  lastUpdated,
+  loading,
+  handleCountComplete,
+  handleSourcePress,
+}: Props) => {
+  return (
+    <Container>
+      <TitleText>{title}</TitleText>
+
+      {loading ? (
+        <Animator
+          type="opacity"
+          initialValue={0.33}
+          finalValue={1}
+          shouldAnimateIn
+          shouldRepeat
+          duration={750}>
+          <LoadingContainer />
+        </Animator>
+      ) : (
+        <Counter
+          key={end} // mount new if end changes
+          start={start}
+          end={end}
+          time={2000}
+          easing="linear"
+          style={styles.counter}
+          onComplete={handleCountComplete}
+        />
+      )}
+
+      <FooterText>
+        {!loading
+          ? `Last Updated: ${lastUpdated}`
+          : 'Fetching the latest data...'}
+      </FooterText>
+
+      {!loading ? (
+        <SourceButton onPress={handleSourcePress}>
+          <SourceText>
+            Source <Icon name="open-in-new" />
+          </SourceText>
+        </SourceButton>
+      ) : (
+        <View style={styles.sourceButtonPlaceholder} />
+      )}
+    </Container>
+  );
+};
+
+export default CounterView;
--- src/components/CounterView/index.tsx
+++ src/components/CounterView/index.tsx
@@ -1,80 +1,9 @@
 import React from 'react';
-import Counter from 'react-native-counter';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import {View} from 'react-native';
-import Animator from 'react-native-simple-animators';
 
-import styles, {
-  Container,
-  TitleText,
-  FooterText,
-  SourceText,
-  SourceButton,
-  LoadingContainer,
-} from './styles';
+import CounterView, {Props} from './CounterView';
 
-interface Props {
-  title: string;
-  start: Number;
-  end: Number;
-  lastUpdated: string;
-  loading?: boolean;
-  handleCountComplete: () => void;
-  handleSourcePress: () => void;
-}
-
-const CounterView = ({
-  title,
-  start,
-  end,
-  lastUpdated,
-  loading,
-  handleCountComplete,
-  handleSourcePress,
-}: Props) => {
-  return (
-    <Container>
-      <TitleText>{title}</TitleText>
-
-      {loading ? (
-        <Animator
-          type="opacity"
-          initialValue={0.33}
-          finalValue={1}
-          shouldAnimateIn
-          shouldRepeat
-          duration={750}>
-          <LoadingContainer />
-        </Animator>
-      ) : (
-        <Counter
-          key={end} // mount new if end changes
-          start={start}
-          end={end}
-          time={2000}
-          easing="linear"
-          style={styles.counter}
-          onComplete={handleCountComplete}
-        />
-      )}
-
-      <FooterText>
-        {!loading
-          ? `Last Updated: ${lastUpdated}`
-          : 'Fetching the latest data...'}
-      </FooterText>
-
-      {!loading ? (
-        <SourceButton onPress={handleSourcePress}>
-          <SourceText>
-            Source <Icon name="open-in-new" />
-          </SourceText>
-        </SourceButton>
-      ) : (
-        <View style={styles.sourceButtonPlaceholder} />
-      )}
-    </Container>
-  );
+const CounterViewContainer = ({...props}: Props) => {
+  return <CounterView {...props} />;
 };
 
-export default CounterView;
+export default CounterViewContainer;
