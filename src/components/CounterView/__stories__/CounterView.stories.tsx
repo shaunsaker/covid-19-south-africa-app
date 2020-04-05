bf560ad9cd073f40df60b84d5bac30df0896a85f
shaunsaker <shaun@MACs-MacBook-Pro.local>
2020-04-05 08:29:23 +0200

    Add CounterView as component

---
+++ src/components/CounterView/__stories__/CounterView.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import {View} from 'react-native';
+
+import CounterView from '..';
+import {colors} from '../../../styleConstants';
+
+const title = 'Confirmed Cases';
+const start = 0;
+const end = 1170;
+const lastUpdated = '27 March 2020, 20h14';
+const handleSourcePress = () => console.log('Source pressed');
+const handleCountComplete = () => console.log('Count complete');
+const defaultProps = {
+  title,
+  start,
+  end,
+  lastUpdated,
+  handleSourcePress,
+  handleCountComplete,
+};
+
+storiesOf('Components|CounterView', module)
+  .addDecorator((storyFn: any) => (
+    <View style={{backgroundColor: colors.primaryLight}}>{storyFn()}</View>
+  ))
+  .add('default', () => <CounterView {...defaultProps} />)
+  .add('loading', () => (
+    <CounterView {...defaultProps} start={0} end={0} lastUpdated="" loading />
+  ));
