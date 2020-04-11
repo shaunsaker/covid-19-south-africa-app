9aa134105e3293f57b48f9ccb8a6be6d5148685e
Shaun Saker <sakershaun@gmail.com>
2020-04-11 14:24:22 +0200

    Up the contrast of the text colors

--- src/styleConstants/colors/index.ts
+++ src/styleConstants/colors/index.ts
@@ -4,7 +4,6 @@ export const colors = {
   primaryMedium: 'rgb(28,31,35)',
   secondary: 'rgb(194,37,15)',
   secondaryLight: 'rgb(237,78,18)',
-  primaryText: 'rgb(177,179,181)',
-  secondaryText: 'rgb(129,134,137)',
-  whiteText: 'rgba(255, 255, 255, 0.9)',
+  primaryText: 'rgba(255, 255, 255, 0.9)',
+  secondaryText: 'rgb(177,179,181)',
 };
