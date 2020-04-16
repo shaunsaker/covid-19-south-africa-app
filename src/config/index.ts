1ac57c486b1c1f0a6225e798f715cfb36c097492
Shaun Saker <sakershaun@gmail.com>
2020-04-16 20:37:20 +0200

    Add countriesApi and countriesImages url to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,10 @@
 export const build = 7;
 export const code = 26;
 export const email = 'sakershaun@gmail.com';
+export const countriesApi =
+  'https://coronavirus-tracker-api.herokuapp.com/v2/locations';
+export const countriesImagesUrl =
+  'https://raw.githubusercontent.com/hjnilsson/country-flags/master/png250px/';
 
 export * from './snackbar';
 export * from './tabs';
