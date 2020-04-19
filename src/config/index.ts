25c4e6d8259d179e19e9e2c5cf71cae6c2f22c75
Shaun Saker <sakershaun@gmail.com>
2020-04-19 11:20:22 +0200

    Update code version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const build = 7;
-export const code = 26;
+export const code = 27;
 export const email = 'sakershaun@gmail.com';
 export const countriesApi =
   'https://coronavirus-tracker-api.herokuapp.com/v2/locations';
