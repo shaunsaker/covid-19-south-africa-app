df7b1dd557f8a16a8e26e7a2e49facd569f212ce
Shaun Saker <sakershaun@gmail.com>
2020-04-23 17:04:43 +0200

    Update code version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const build = 7;
-export const code = 33;
+export const code = 34;
 export const email = 'sakershaun@gmail.com';
 export const countriesApi = 'https://corona.lmao.ninja/v2';
 export const countriesApiSource = 'https://github.com/NovelCOVID/API';
