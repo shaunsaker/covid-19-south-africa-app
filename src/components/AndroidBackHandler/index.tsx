d0cc3d4779e2ed22113426cdcbffe0a4599f8823
Shaun Saker <sakershaun@gmail.com>
2020-05-19 08:10:11 +0200

    Pop page if android back press

--- src/components/AndroidBackHandler/index.tsx
+++ src/components/AndroidBackHandler/index.tsx
@@ -1,22 +1,28 @@
 import {useEffect, useCallback} from 'react';
 import {BackHandler} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
-import {sideMenuIsOpenSelector} from '../../store/selectors';
+import {Actions} from 'react-native-router-flux';
+import {sideMenuIsOpenSelector, getCurrentScene} from '../../store/selectors';
 import {setSideMenu} from '../../store/actions';
 
 const AndroidBackHandler = () => {
   const sideMenuOpen = useSelector(sideMenuIsOpenSelector);
+  const currentScene = useSelector(getCurrentScene);
+  const isNotTabScene = !currentScene.includes('_');
   const dispatch = useDispatch();
 
   const onHardwareBackPress = useCallback(() => {
     if (sideMenuOpen) {
       dispatch(setSideMenu(false));
       return true;
+    } else if (isNotTabScene) {
+      Actions.pop();
+      return true;
     } else {
       BackHandler.exitApp();
       return false;
     }
-  }, [dispatch, sideMenuOpen]);
+  }, [dispatch, sideMenuOpen, isNotTabScene]);
 
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', onHardwareBackPress);
